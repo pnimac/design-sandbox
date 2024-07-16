d5b37ae48d41dd4cc6289fc66e389e5ac017ed95
“pnimac” <poornima.chauhan.2019@gmail.com>
2024-07-16 18:23:53 +1000

    Initial commit

---
+++ src/main/java/com/pnimac/design/desigsandbox/DesigsandboxApplication.java
@@ -0,0 +1,13 @@
+package com.pnimac.design.desigsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesigsandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesigsandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnimac/design/desigsandbox/DesigsandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnimac.design.desigsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesigsandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
